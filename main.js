499c3e52186d2fbdae2ce875778b42ac2e963c24
TeshioSaira <capitalgain5.fukyoyo12ac@gmail.com>
2025-10-13 19:28:41 +0900

    back and infoDiv

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
 var account_move = false;
+var account_move_return = false;
+var informationChanger_disappear = false;
+var informationChanger_disappear_fitst = false;
 var account_startTime = 0;
+var informationChanger_startTime = 0;
 var account_count = 0;
+var informationChanger_count = 0;
 
 window.onload = firstScript;
 
 function firstScript(){
     account_count = document.getElementsByClassName('account_icon_img').length;
+    informationChanger_count = document.getElementsByClassName('informationChangerItem').length;
     modeSelect(0);
 }
 
 function modeSelect(mode){
+    console.log(mode);
+    var displayWidth = window.outerWidth;
+    console.log(displayWidth);
     var infrontInformationItem = document.getElementsByClassName('infrontInformationItem');
     for(var i = 0; i < infrontInformationItem.length; i++){
+        if(infrontInformationItem[i].getAttribute('style').split('animation').length > 1){
+            if(infrontInformationItem[i].getAttribute('style').split('disappear').length > 1){
+                infrontInformationItem[i].removeAttribute('style');
+                infrontInformationItem[i].setAttribute('style', 'display: none;');
+            }else{
+                infrontInformationItem[i].removeAttribute('style');
+                infrontInformationItem[i].setAttribute('style', 'animation: 1s ease-out forwards infrontInformationItem_disappear;');
+            }
+        }
         if(mode == i){
             infrontInformationItem[i].removeAttribute('style');
+            infrontInformationItem[i].setAttribute('style', 'animation: 1s ease-out forwards infrontInformationItem_appear;');
+        }
+    }
+    var informationChangerItem = document.getElementsByClassName('informationChangerItem');
+    for(var i = 0; i < informationChangerItem.length; i++){
+        if(i + 1 == mode){
+            var width = informationChangerItem[i].clientWidth / displayWidth * 100;
+            var attribute = informationChangerItem[i].getAttribute('style');
+            informationChangerItem[i].removeAttribute('style');
+            informationChangerItem[i].setAttribute('style', attribute + ' width: ' + width + '%; animation: 1s ease-out forwards informationChangerItem_appear;');
+            informationChangerItem[i].children[0].removeAttribute('style');
+            informationChangerItem[i].children[0].setAttribute('style', 'animation: 1s ease-out forwards informationChangerItem_p_appear;');
         }else{
-            infrontInformationItem[i].removeAttribute('style');
-            infrontInformationItem[i].setAttribute('style', 'display: none;');
+            var attribute = informationChangerItem[i].getAttribute('style');
+            informationChangerItem[i].removeAttribute('style');
+            informationChangerItem[i].setAttribute('style', attribute + ' animation: 1s ease-out forwards informationChangerItem_disappear;');
+            informationChangerItem[i].children[0].removeAttribute('style');
         }
     }
     if(mode == 0){
         account_move = true;
+        account_startTime = new Date().getTime();
+    }else{
+        account_move_return = true;
         account_startTime = new Date().getTime();
     }
 }
@@ -43,25 +78,97 @@ function openYouTube(){
     }
 }
 
+function returnHome(){
+    if(!informationChanger_disappear){
+        informationChanger_disappear_fitst = true;
+        informationChanger_disappear = true;
+        informationChanger_startTime = new Date().getTime();
+    }
+}
+
 setInterval(() => {
-    var displayWidth = window.innerWidth;
-    var displayHeight = window.innerHeight;
+    var displayWidth = window.outerWidth;
+    var displayHeight = window.outerHeight;
     if(account_move){
         var account_time = new Date().getTime() - account_startTime;
         for(var i = 0; i < account_count; i++){
-            var radian = Math.PI * ((0.5 + (i + 1) / (account_count + 1)) * account_time / 2000 + 0.5);
-            var top = (displayHeight / 2 - Math.sin(radian) * displayWidth / 9 - displayWidth * 0.075) / displayHeight * 100;
+            var radian = Math.PI * ((0.5 + (i + 1) / (account_count + 1)) * Math.sqrt(1 - Math.pow(1 - account_time / 1000, 2)) + 0.5);
+            var top = (displayHeight / 2 - Math.sin(radian) * displayWidth / 20 - displayWidth * 0.075) / displayHeight * 100;
             var left = (1 + Math.cos(radian)) * 50 - 7.5;
             var opacity = 1;
-            var scale = (3 - Math.sin(radian)) / 4;
-            if(radian < Math.PI){
+            var scale = (0 - Math.sin(radian)) * 2;
+            if(radian % (Math.PI * 2) < Math.PI){
                 opacity = 0.5;
             }
             document.getElementsByClassName('account_icon_img')[i].removeAttribute('style');
             document.getElementsByClassName('account_icon_img')[i].setAttribute('style', 'top: ' + top + '%; left: ' + left + '%; opacity: ' + opacity + '; transform: scale(' + scale + ');');
         }
-        if(account_time >= 2000){
+        for(var i = 0; i < informationChanger_count; i++){
+            if(i < informationChanger_count / 2){
+                var radian = Math.PI * (1 - (1 + i) / (1 + informationChanger_count) * Math.sqrt(1 - Math.pow(1 - account_time / 1000, 2)));
+                var top = ((displayHeight / 2 - displayWidth * 0.125) * (1 - Math.sin(radian))) / displayHeight * 100;
+                var left = (1 + Math.cos(radian)) * 45;
+                var opacity = radian / (Math.PI * 0.1);
+                document.getElementsByClassName('informationChangerItem')[i].removeAttribute('style');
+                document.getElementsByClassName('informationChangerItem')[i].setAttribute('style', 'top: ' + top + '%; left: ' + left + '%; opacity: ' + opacity + ';');
+            }else{
+                var radian = Math.PI * (1 - (informationChanger_count - i) / (1 + informationChanger_count) * Math.sqrt(1 - Math.pow(1 - account_time / 1000, 2)));
+                var top = ((displayHeight / 2 - displayWidth * 0.125) * (1 - Math.sin(radian))) / displayHeight * 100;
+                var right = (1 + Math.cos(radian)) * 45;
+                var opacity = radian / (Math.PI * 0.1);
+                document.getElementsByClassName('informationChangerItem')[i].removeAttribute('style');
+                document.getElementsByClassName('informationChangerItem')[i].setAttribute('style', 'top: ' + top + '%; right: ' + right + '%; opacity: ' + opacity + ';');
+            }
+        }
+        if(account_time >= 1000){
             account_move = false;
+        }
+    }
+    if(account_move_return){
+        var account_time = new Date().getTime() - account_startTime;
+        for(var i = 0; i < account_count; i++){
+            var radian = Math.PI * ((0.5 + (i + 1) / (account_count + 1)) + (0.5 + (account_count - i) / (account_count + 1)) * Math.sqrt(1 - Math.pow(1 - account_time / 1000, 2)) + 0.5);
+            var top = (displayHeight / 2 - Math.sin(radian) * displayWidth / 20 - displayWidth * 0.075) / displayHeight * 100;
+            var left = (1 + Math.cos(radian)) * 50 - 7.5;
+            var opacity = 1;
+            var scale = (3 - Math.sin(radian)) / 4;
+            if(radian % (Math.PI * 2) < Math.PI){
+                opacity = 0.5;
+            }
+            document.getElementsByClassName('account_icon_img')[i].removeAttribute('style');
+            document.getElementsByClassName('account_icon_img')[i].setAttribute('style', 'top: ' + top + '%; left: ' + left + '%; opacity: ' + opacity + '; transform: scale(' + scale + ');');
+        }
+        if(account_time >= 1000){
+            account_move_return = false;
+            var infrontInformationItem = document.getElementsByClassName('infrontInformationItem');
+            infrontInformationItem[0].setAttribute('style', 'display: none;');
+        }
+    }
+    if(informationChanger_disappear){
+        var informationChangerItem = document.getElementsByClassName('informationChangerItem');
+        var informationChanger_disappear_time = new Date().getTime() - informationChanger_startTime;
+        var now_style = "";
+        for(var i = 0; i < informationChangerItem.length; i++){
+            now_style = informationChangerItem[i].getAttribute('style');
+            if(now_style.split('_appear').length > 1){
+                if(informationChanger_disappear_fitst){
+                    now_style = now_style.split(' opacity')[0] + ' width' + now_style.split(' opacity')[1].split('; width')[1];
+                    informationChanger_disappear_fitst = false;
+                }else{
+                    now_style = now_style.split(' opacity')[0];
+                }
+                var opacity = (1000 - informationChanger_disappear_time) / 1000;
+                informationChangerItem[i].removeAttribute('style');
+                informationChangerItem[i].setAttribute('style', now_style + ' opacity: ' + opacity + ';');
+                if(informationChanger_disappear_time >= 1000){
+                    informationChanger_disappear = false;
+                    informationChangerItem[i].setAttribute('style', 'display: none;');
+                }
+            }
+        }
+        if(informationChanger_disappear_time >= 1000){
+            informationChanger_disappear = false;
+            modeSelect(0);
         }
     }
 }, 10);
