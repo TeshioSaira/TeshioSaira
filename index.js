2a630239684f94ad830999d92373b9f4904b78a7
TeshioSaira <capitalgain5.fukyoyo12ac@gmail.com>
2026-08-10 16:37:02 +0900

    fixJs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function getNewestVideo(){
-    const response = await fetch('data.json');
+    const response = await fetch('videos.json');
     const data = await response.json();
     const videos = data["videos"];
     var newestDate = new Date('2001-01-01T08:00:16+00:00');
