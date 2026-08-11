5594068aff0c44fd2cb8f543b9985185f7748e5f
TeshioSaira <capitalgain5.fukyoyo12ac@gmail.com>
2026-08-12 08:49:13 +0900

    removeHeader

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 async function load_header_footer() {
-    const header = await fetch("header.html");
-    document.getElementById("header").innerHTML = await header.text();
     const footer = await fetch("footer.html");
     document.getElementById("footer").innerHTML = await footer.text();
 }
